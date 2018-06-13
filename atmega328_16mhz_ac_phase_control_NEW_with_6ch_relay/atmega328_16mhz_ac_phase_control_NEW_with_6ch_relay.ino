c3d8ded67a3d1aa25db300e0920aebdf2da75aa1
ashloverscn <ashloverscn@gmail.com>
2018-06-14 03:44:57 +0530

    show_fan_speed(dim_value);//reloading display after every process
    +
    BUG Fix:added in main loop to refresh display after every complete job
    +
    show_fan_speed(dim_value);//reloading display after every process

--- atmega328_16mhz_ac_phase_control_NEW_with_6ch_relay/atmega328_16mhz_ac_phase_control_NEW_with_6ch_relay.ino
+++ atmega328_16mhz_ac_phase_control_NEW_with_6ch_relay/atmega328_16mhz_ac_phase_control_NEW_with_6ch_relay.ino
@@ -22,6 +22,12 @@ int spd[14]={600,500,480,450,400,380,350,300,250,180,150,80,50,1};//our good sta
 
 int value = 0; 
 
+const int latchPin = A4;//Pin connected to ST_CP of 74HC595
+const int clockPin = A5;//Pin connected to SH_CP of 74HC595 
+const int dataPin = A3; //Pin connected to DS of 74HC595 
+//display 0,1,2,3,4,5,6,7,8,9,A,b,C,d,E,F,g,H
+int datArray[18] = {252, 96, 218, 242, 102, 182, 190, 224, 254, 246, 239, 63, 157, 123, 159, 143, 247, 111};
+
 Adafruit_NECremote remote(IRpin);
 
 int L1=LOW;int L2=LOW;int L3=LOW;int L4=LOW;int L5=LOW;int L6=LOW;int L7=LOW;int L8=LOW;
@@ -32,6 +38,7 @@ int dim_value = 0;
 
 void setup() {
   setup_io();//set pins as output for relays and triac
+  //reboot_complete_indication();
   dim_value = EEPROM.read(0);//re innitialize fan value
   atmega328_16mhz_ac_phase_control.init();
   //atmega328_16mhz_ac_phase_control.uninit();
@@ -39,7 +46,7 @@ void setup() {
   last_value_initialize();//inialize last value from epprom
   //Serial.begin(115200);
   //Serial.println("Ready to decode IR!");
-
+   //set pins to output
 }
 
 
@@ -68,7 +75,7 @@ void loop() {
   } else {
     //Serial.println("Timed out waiting!");
   }*/
-  /*#########################################################################################################*/
+  show_fan_speed(dim_value);//reloading and refresh display after every process
 }
  /*#########################################################################################################*/ 
  
@@ -78,6 +85,10 @@ void setup_io()
   {
     pinMode(i,OUTPUT);
   }
+  //shift register output pins
+  pinMode(latchPin,OUTPUT);
+  pinMode(clockPin,OUTPUT);
+  pinMode(dataPin,OUTPUT);
 }
 
  /*#########################################################################################################*/ 
@@ -93,6 +104,8 @@ void setup_io()
     {
     digitalWrite(i,EEPROM.read(i-3));
     }
+    //reload fan speed display
+    show_fan_speed(dim_value);
   }    
   
  /*#########################################################################################################*/ 
@@ -163,7 +176,8 @@ void relay(int key_pressed)
       {
         dim_value -= 1 ;
         atmega328_16mhz_ac_phase_control.set_ac_power(spd[dim_value]);//speed[0 to 19] or 0 to 360 through 400 
-        EEPROM.write(0, dim_value);        
+        EEPROM.write(0, dim_value);
+        show_fan_speed(dim_value);        
       }
     break;
         case 6: 
@@ -173,6 +187,7 @@ void relay(int key_pressed)
        dim_value += 1;
        atmega328_16mhz_ac_phase_control.set_ac_power(spd[dim_value]);//speed[0 to 19] or 0 to 360 through 400 
        EEPROM.write(0, dim_value);
+       show_fan_speed(dim_value);
       }
     break;
 
@@ -183,4 +198,29 @@ void relay(int key_pressed)
 }
 
  /*#########################################################################################################*/
-  
+
+ void show_fan_speed(int value)
+ {  digitalWrite(latchPin,LOW);
+    //clear the old date in the shift register
+    shiftOut(dataPin,clockPin,LSBFIRST,~(0B00000000));
+    //now write new data to the register
+    shiftOut(dataPin,clockPin,LSBFIRST,~(datArray[value]));
+    //inverting data for common andode display
+    //return the latch pin high to signal chip that it 
+    //no longer needs to listen for information
+    digitalWrite(latchPin,HIGH); //pull the latchPin to save the data
+ }
+/*
+ void reboot_complete_indication()
+ {
+  digitalWrite(latchPin,LOW);
+  //clear the old date in the shift register
+  shiftOut(dataPin,clockPin,LSBFIRST,~(0B00000000));
+    //now write new data to the register
+  shiftOut(dataPin,clockPin,LSBFIRST,~(0B00000001));//display a dot when boot completes
+  //inverting data for common andode display
+  //return the latch pin high to signal chip that it 
+  //no longer needs to listen for information
+  digitalWrite(latchPin,HIGH); //pull the latchPin to save the data
+ }
+*/
